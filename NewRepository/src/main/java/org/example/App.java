e7c07ad9a7db91bebf473901b83c4d38d8af9d9d
KaSuchanek <ksuchanek@gmail.com>
2023-04-20 20:17:29 +0200

    new line

--- NewRepository/src/main/java/org/example/App.java
+++ NewRepository/src/main/java/org/example/App.java
@@ -2,33 +2,28 @@ package org.example;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
 
-        System.out.println( "Hello!" );
-        System.out.println( "Hurra!" );
+        System.out.println("Hello!");
+        System.out.println("Hurra!");
 
-        String[] cities = new String [3];
+        String[] cities = new String[3];
 
         cities[0] = "Praga";
         cities[1] = "Kraków";
         cities[2] = "Londyn";
 
-        
+        for (i = 0; i < cities.length; i++) {
+            String city = cities[i];
+            System.out.println((i + 1) + ". " + city);
+        }
 
-
-
-
-
+        }
 
 
     }
-
-
 
 
 }
